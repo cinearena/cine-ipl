0a00bf4f385ae385edc9ef36d21eb24d24209587
Cine Arena <asifansarieng@gmail.com>
2026-01-14 17:19:37 +0530

    Update inline_scripts.js

--- js/inline_scripts.js
+++ js/inline_scripts.js
@@ -367,7 +367,7 @@
         document.querySelector('.match-select').style.display = 'block';
         const matchSelect = document.getElementById('matchSelect');
 
-        fetch('https://cors-proxy.fly.dev/https://ipelskcricinfo.saqlainhaider8198.workers.dev')
+        fetch('https://api.allorigins.win/raw?url=https://ipelskcricinfo.saqlainhaider8198.workers.dev')
           .then(response => response.json())
           .then(matches => {
             matchSelect.innerHTML = '';
@@ -389,7 +389,7 @@
         return;
       }
 
-      fetch('https://cors-proxy.fly.dev/https://ipelskcricinfo.saqlainhaider8198.workers.dev')
+      fetch('https://api.allorigins.win/raw?url=https://ipelskcricinfo.saqlainhaider8198.workers.dev')
         .then(r => r.json())
         .then(matches => {
           if (matches && matches[matchId]) {
